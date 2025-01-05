871149053bccdade99149ad08527990116f08c14
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2025-01-05 19:18:28 +0530

    organize minimal apis

---
+++ MinimalApiDemo/Config/EndpointsMapper.cs
@@ -0,0 +1,11 @@
+﻿using MinimalApiDemo.Features.Pet;
+
+namespace MinimalApiDemo.Config;
+
+public static class EndpointsMapper
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        app.MapPetEndpoints();
+    }
+}
---
+++ MinimalApiDemo/Features/Pet/PetDto.cs
@@ -0,0 +1,10 @@
+﻿using MinimalApiDemo.Common.Enums;
+
+namespace MinimalApiDemo.Features.Pet;
+
+public record PetDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public Gender Gender { get; set; }
+}
