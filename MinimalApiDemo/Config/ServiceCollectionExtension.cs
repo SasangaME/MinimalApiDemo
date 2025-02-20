964fbf248809cc2ccb004649999c05fe079027d1
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2025-02-20 22:59:47 +0530

    MongoDB connection

---
+++ MinimalApiDemo/Config/ServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using MinimalApiDemo.Database;
+
+namespace MinimalApiDemo.Config;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection RegisterDependencies(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<DbContext>()
+            .AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+    }
+}
